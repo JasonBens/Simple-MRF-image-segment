d770b3cc45cf29331fc64cb9ba37f245b7521c0e
Neophox <Jason.L.Bens@gmail.com>
2014-03-08 12:58:13 -0800

    Fix issue 2
    Fixed issue 2.  Previously, the anneal function wasn't guaranteed to
    change pixels.  Now it is.

--- annealingfcn.m
+++ annealingfcn.m
@@ -1,18 +1,18 @@
-function annealedLabel = annealingfcn(optimValues, problem, numLabel)
+function newLabel = annealingfcn(optimValues, problem, numLabel)
   
   % Annealing scheme is similar to annealingfast, where the number of
   % updated labels is proportional to the current temperature.
   
+  newLabel = optimValues.x;
+  
   initPercent = 1;
   numUpdate = max(1, ...
     round(problem.nvar / (initPercent * (optimValues.iteration + 1))));
-  
+
   %numUpdate = 1;
   
-  % TODO:  This can generate the same label, meaning no update.  Set of
-  % valid labels should be XOR of current label and all labels.
-  updateVect = randi(problem.nvar, numUpdate, 1);
-  annealedLabel = optimValues.x;
-  annealedLabel(updateVect) = randi(numLabel, numUpdate, 1);
-  
+  update = randi(problem.nvar, numUpdate, 1);
+  offset = randi(numLabel - 1, numUpdate, 1) - 1;
+  newLabel(update) = 1 + mod(newLabel(update) + offset, numLabel);
+    
 end
